d3d9acff9326f88e757d926a2178c18c477e8640
Jake Kim <imjakekim@gmail.com>
2022-08-08 12:36:49 -0700

    created enigma tests

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,8 +1,15 @@
 require "./lib/enigma"
 
 RSpec.describ Enigma do
-  let(:enigma) {Enigma.new()}
+  let(:enigma) {Enigma.new}
   it "exists" do
     expect(enigma).to be_an(Enigma)
+  end
+
+  it "can encrypt" do
+    expected_hash = { encryption: "keder ohulw",
+                      key: "02715",
+                      date: "040895" }
+    expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected_hash)
   end
 end
