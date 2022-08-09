1f048390e3ef913e4c6854a8b078561eb1f1b7ea
Jake Kim <imjakekim@gmail.com>
2022-08-08 19:03:00 -0700

    created decryption test

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -12,4 +12,11 @@ RSpec.describe Enigma do
                       date: "040895" }
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected_hash)
   end
+
+  it "can decrypt" do
+    expected_hash = { decryption: "hello world",
+                      key: "02715",
+                      date: "040895" }
+    expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected_hash)
+  end
 end
