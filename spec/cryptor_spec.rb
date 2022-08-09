53aa79b5daf73da1a112ede3bbfb26c33481cfc8
Jake Kim <imjakekim@gmail.com>
2022-08-09 15:37:13 -0700

    created test for encryption method

--- spec/cryptor_spec.rb
+++ spec/cryptor_spec.rb
@@ -1,9 +1,13 @@
 require './lib/cryptor'
 RSpec.describe Cryptor do
   let(:dummy_class) { Class.new { extend Cryptor } }
+  let(:char_set) {("a".."z").to_a << " "}
   it "can calculate the finals shifts" do
     expect(dummy_class.final_shifts("02715", "040895")).to eq([3, 27, 73, 20])
   end
 
-  
+  it "can encrypt a message" do
+    expect(dummy_class.encryption("hello", [3,27,73,20], char_set)).to eq("keder")
+  end
+
 end
