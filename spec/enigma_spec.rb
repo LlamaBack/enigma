2887857d15a6d8daca8397cd544bad6a5192c9ac
Jake Kim <imjakekim@gmail.com>
2022-08-08 19:17:31 -0700

    tried stubbing

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -6,17 +6,25 @@ RSpec.describe Enigma do
     expect(enigma).to be_an(Enigma)
   end
 
-  it "can encrypt" do
+  it "can encrypt with all arguments" do
     expected_hash = { encryption: "keder ohulw",
                       key: "02715",
                       date: "040895" }
     expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected_hash)
   end
 
-  it "can decrypt" do
+  it "can decrypt with all arguments" do
     expected_hash = { decryption: "hello world",
                       key: "02715",
                       date: "040895" }
     expect(enigma.decrypt("keder ohulw", "02715", "040895")).to eq(expected_hash)
+  end
+
+  it "can encrypt without given date" do
+    expected_hash = { encryption: "keder ohulw",
+                      key: "02715",
+                      date: "040895" }
+    allow_any_instance_of(Data).to recieve(:strftime).and_return
+    expect(enigma.encrypt("hello world", "02715")).to eq(expected_hash)
   end
 end
