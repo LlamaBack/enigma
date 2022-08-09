0a2a004589f27d6b070605f4e17ee614e26c86ee
Jake Kim <imjakekim@gmail.com>
2022-08-08 19:04:55 -0700

    finished decryption

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -11,9 +11,25 @@ class Enigma
     shifts = [key[0..1].to_i + offsets[0].to_i, key[1..2].to_i + offsets[1].to_i,
             key[2..3].to_i + offsets[2].to_i, key[3..4].to_i + offsets[3].to_i]
     encryption = message.chars.map.with_index { |char, index|
-      @character_set[(@character_set.index(char) + shifts[index % 4]) % 27]
+      if @character_set.include?(char)
+        @character_set[(@character_set.index(char) + shifts[index % 4]) % 27]
+      end
     }.join
     # require "pry"; binding.pry
     {encryption: encryption, key: key, date: date}
+  end
+
+  def decrypt(message, key = @random_key, date = Date.today.strftime("%d%m%y"))
+    message = message.downcase
+    offsets = ((date.to_i ** 2) % 10000).to_s
+    shifts = [key[0..1].to_i + offsets[0].to_i, key[1..2].to_i + offsets[1].to_i,
+            key[2..3].to_i + offsets[2].to_i, key[3..4].to_i + offsets[3].to_i]
+    decryption = message.chars.map.with_index { |char, index|
+      if @character_set.include?(char)
+        @character_set[(@character_set.index(char) - shifts[index % 4]) % 27]
+      end
+    }.join
+    # require "pry"; binding.pry
+    {decryption: decryption, key: key, date: date}
   end
 end
